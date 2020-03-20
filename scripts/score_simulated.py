cfbcdfeebcc7fcedd87eaf4073d4565656b7ee7d
mparker2 <mtparker@dundee.ac.uk>
2020-03-20 10:43:52 +0000

    updated sim aln scoring and rerun pipeline & notebooks

--- scripts/score_simulated.py
+++ scripts/score_simulated.py
@@ -1,4 +1,5 @@
 import re
+import warnings
 from collections import defaultdict, Counter
 import numpy as np
 import click
@@ -16,22 +17,22 @@ def parse_exons(record):
     exstarts = np.fromstring(record[11], sep=',', dtype=int) + start
     exends = exstarts + np.fromstring(record[10], sep=',', dtype=int)
     exons = np.dstack([exstarts, exends])[0]
-    return exons
+    return start, end, exons
 
 
 def get_bed12_linked_splicing(bed_fn):
-    linked_splices = defaultdict(set)
     with open(bed_fn) as bed:
         for record in bed:
             record = record.split()
             chrom = record[0]
             strand = record[5]
             read_id = record[3]
-            exons = parse_exons(record)
+            start, end, exons = parse_exons(record)
             introns = exons_to_introns(exons)
+            linked_splices = set()
             for inv in introns:
-                linked_splices[read_id].add(f'{chrom}:{inv[0]}-{inv[1]}({strand})')
-    return linked_splices
+                linked_splices.add(f'{chrom}:{inv[0]}-{inv[1]}({strand})')
+            yield read_id, linked_splices
 
 
 def get_gtf_transcript_id(attrs):
@@ -59,9 +60,10 @@ def parse_gtf_exon_invs(gtf_fn):
 
 
 def get_gtf_linked_splicing(gtf_fn):
-    linked_splices = defaultdict(set)
+    linked_splices = {}
     for transcript_id, chrom, strand, exons in parse_gtf_exon_invs(gtf_fn):
         introns = exons_to_introns(exons)
+        linked_splices[transcript_id] = set()
         for inv in introns:
             linked_splices[transcript_id].add(f'{chrom}:{inv[0]}-{inv[1]}({strand})')
     return linked_splices
@@ -69,10 +71,9 @@ def get_gtf_linked_splicing(gtf_fn):
 
 def score_sim_alignment(aln_bed_fn, reference_splices, remove_version=False):
     ref_splice_inv = {frozenset(v): k for k, v in reference_splices.items()}
-    aln_splices = get_bed12_linked_splicing(aln_bed_fn)
     res = {}
     quant_res = Counter()
-    for read_id, splice in aln_splices.items():
+    for read_id, splice in get_bed12_linked_splicing(aln_bed_fn):
         splice = frozenset(splice)
         transcript_of_origin = re.search('^(.+)_sim\d+', read_id).group(1)
         if remove_version:
@@ -82,19 +83,28 @@ def score_sim_alignment(aln_bed_fn, reference_splices, remove_version=False):
             # in the header (format TRANSCRIPT_ID.VERSION)
             # whereas the gtf does not
             transcript_of_origin = transcript_of_origin.split('.')[0]
-        correct_splicing = reference_splices[transcript_of_origin]
-        if splice == correct_splicing:
-            res[read_id] = 1 # splice is correct
+        try:
+            correct_splicing = reference_splices[transcript_of_origin]
+        except KeyError:
+            warnings.warn(f'Simulated transcript {transcript_of_origin} not in GTF')
+            continue
+        if not len(splice) and not len(correct_splicing):
+            # read is correctly unspliced
+            res[read_id] = 'unspliced_correctly'
             quant_res[transcript_of_origin] += 1
-        elif not len(splice):
-            res[read_id] = 0 # read is incorrectly unspliced
-            # don't add to any transcript
-        elif splice in ref_splice_inv:
-            res[read_id] = 2 # read maps to incorrect transcript
-            quant_res[ref_splice_inv[splice]] += 1
+        elif not len(splice) and len(correct_splicing):
+            res[read_id] = 'unspliced_incorrectly' # read is incorrectly unspliced
         else:
-            res[read_id] = 0 # read is incorrectly spliced
-            # no annotated transcript to add to
+            # both read and reference are spliced
+            if splice == correct_splicing:
+                res[read_id] = 'spliced_correctly' # splice is correct
+                quant_res[transcript_of_origin] += 1
+            elif splice in ref_splice_inv:
+                res[read_id] = 'spliced_incorrectly_annot' # read maps to incorrect transcript
+                quant_res[ref_splice_inv[splice]] += 1
+            else:
+                res[read_id] = 'spliced_incorrectly_unannot' # read is incorrectly spliced
+                # no annotated transcript to add to
     return res, quant_res
 
 
@@ -106,7 +116,10 @@ def score_sim_alignment(aln_bed_fn, reference_splices, remove_version=False):
 @click.option('-r', '--remove-ensembl-transcript-version', default=False, is_flag=True)
 def main(bed12_fn, annot_gtf_fn, output_fn, output_quant_fn, remove_ensembl_transcript_version):
     reference_splices = get_gtf_linked_splicing(annot_gtf_fn)
-    res, quant_res = score_sim_alignment(bed12_fn, reference_splices, remove_ensembl_transcript_version)
+    res, quant_res = score_sim_alignment(
+        bed12_fn, reference_splices,
+        remove_ensembl_transcript_version
+    )
     with open(output_fn, 'w') as f:
         for read_id, classi in res.items():
             f.write(f'{read_id}\t{classi}\n')
